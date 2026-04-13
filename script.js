a25317e2e834fc271e31f0a1ee964377e464f8ce
Project-Second-Voice <projectsecondvoice@gmail.com>
2026-04-12 20:48:36 -0700

    Add archive story: Jose Gregorio Gonzalez's Story

--- script.js
+++ script.js
@@ -664,6 +664,32 @@ const stories = [
     summary: "People like Brenda Martinez emphasize a need for immigration policies to change. Brenda was taken to the U.S. when she was just a toddler, and having to accept the fact that she isn’t like the rest of her peers is heartbreaking. Martinez is not a criminal, nor has she gotten any deportation orders, yet she still has no clear path to citizenship. The only path she was offered was due to her husband's sponsorship as an American citizen, but this can take years before being accepted, something Martinez and her family can’t risk. Martinez’s only choice is to leave the U.S. with her husband and move to a country she has hardly known, leaving everyone and everything behind. We believe there needs to be a change that better equips immigrants with the necessary tools to be able to make a path towards citizenship that doesn’t have such a length process. Martinez had no choice but to leave the place she grew up in and go to a foreign country. The sources we used for this post are from Newsbusters and CBS News. If you want to learn more about Martinez’s story, we encourage you to read the stories that were published by these organizations. If you or anyone you know, have any stories they would like to share about their experience as immigrants, we would love to hear it. If you feel comfortable, feel free to message us and we will gladly share your story to spread awareness for the millions of immigrants that deserve a change.",
     tiktokLink: "https://www.tiktok.com/@projectsecondvoice/photo/7522323064671980814?is_from_webapp=1&sender_device=pc",
     link: "stories.html#brenda-martinez-story"
+  },
+  {
+    order: 29,
+    slug: "jose-gregorio-gonzalez-story",
+    images: [
+      "stories/jose-gregorio-gonzalez-story/1.png",
+      "stories/jose-gregorio-gonzalez-story/2.png",
+      "stories/jose-gregorio-gonzalez-story/3.png",
+      "stories/jose-gregorio-gonzalez-story/4.png",
+      "stories/jose-gregorio-gonzalez-story/5.png",
+      "stories/jose-gregorio-gonzalez-story/6.png",
+      "stories/jose-gregorio-gonzalez-story/7.png",
+      "stories/jose-gregorio-gonzalez-story/8.png",
+      "stories/jose-gregorio-gonzalez-story/9.png",
+      "stories/jose-gregorio-gonzalez-story/10.png",
+      "stories/jose-gregorio-gonzalez-story/11.png",
+      "stories/jose-gregorio-gonzalez-story/12.png",
+      "stories/jose-gregorio-gonzalez-story/13.png",
+      "stories/jose-gregorio-gonzalez-story/14.png"
+    ],
+    featured: false,
+    isNewest: false,
+    title: "Jose Gregorio Gonzalez's Story",
+    summary: "Today, we are sharing the story of Jose Gregorio Gonzalez. Gonzalez is of Venezuelan nationality and came to the United States to accompany his sick younger brother, who is suffering from end-stage renal disease. This is a disease that affects the kidneys, and his brother is in dire need of a transplant. Gonzalez came to the United States to accompany him in his health care, while also being his kidney donor. By transplanting his kidney through a paired kidney exchange, Gonzalez would be saving both his brother’s and another person’s lives. Gonzalez’s brother has to go through multiple 4-hour dialysis appointments every week to stay alive. Gonzalez would come to the United States and apply for asylum. He would not be granted asylum due to not passing an initial interview. Gonzalez was given a deportation order. While accompanying his brother to a dialysis appointment, Gonzalez would be detained by officers and put in a detention facility to await removal orders. With the advocacy of thousands of individuals, Gonzalez would be given a temporary stay to undergo the kidney transplant procedure, saving two lives. Although Gonzalez may very well be removed this year, even though he has no criminal record, his story serves as a major step forward for immigrant justice and a major victory for humanity and compassion. The sources we used for this post come from the organizations NBC, CNN, TRP Immigrant Justice, Chicago Sun Times, Compassion Crossing, UC Davis Health, ABC 7, The DePaulia, Chicago Tribune, and USA Today. If you would like to learn more about Gonzalez’s story, we encourage you to check out the stories published by these organizations.",
+    tiktokLink: "https://www.tiktok.com/@projectsecondvoice/photo/7588349833350483214?is_from_webapp=1&sender_device=pc",
+    link: "stories.html#jose-gregorio-gonzalez-story"
   }
 ];
 
