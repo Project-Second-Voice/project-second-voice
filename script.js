65f09fdc48969d1b91063dd803b509960b8e8fe6
Project-Second-Voice <projectsecondvoice@gmail.com>
2026-04-13 22:34:55 -0700

    Add archive story: Quinceañera Guests stay home due to recent immigration

--- script.js
+++ script.js
@@ -853,6 +853,33 @@ const stories = [
     summary: "Today, we are sharing the story of Yessenia Ruano. Ruano was originally born in El Salvador, but from a young age, she knew that she wanted to leave her country. Her home was filled with poverty and violence, and after many dangerous situations of theft and mugging, Ruano decided that she would go to the United States. Ruano paid a smuggler to get her into the United States; however, she would never expect to be threatened and held against her will due to a debt she had not paid. After this harrowing experience, Ruano decided to apply for a T-visa, a visa designated for individuals affected by trafficking. Ruano built a life she had always dreamed of in the United States: marrying a Salvadoran man, having two twin daughters, and buying her very own house in Milwaukee. In EL Salvador, she had never gotten the chance to pursue her dream career of being a teacher; however, in America, there was a need for teachers, and she and her husband were able to become teachers. However, due to recent immigration policy changes, Ruano’s pending status was not enough for her to stay in the United States. The fear of deportation was too much to bear for the family, and Ruano and her husband had to make the tough decision of voluntarily moving back to El Salvador. After news of her self-deportation spread, many Milwaukee residents marched and protested for Ruano. Ruano also got many letters of support from her students and co-workers. Ruano also had to sit down and tell her two daughters about the situation and the need to move to a place they had never been to. Ruano hopes that once her daughters turn 18, their family can have another chance at the American dream. The sources used for this post come from the organizations Reuters, 19thn News, Rolling Stone, and Wisconsin Examiner. If you would like to learn more about Ruano’s story, we encourage you to check out the stories published by these organizations.",
     tiktokLink: "https://www.tiktok.com/@projectsecondvoice/photo/7563088369727638797?is_from_webapp=1&sender_device=pc",
     link: "stories.html#yessenia-ruano-story"
+  },
+  {
+    order: 37,
+    slug: "quinceanera-guests-stay-home-due-to-recent-immigration-story",
+    images: [
+      "stories/quinceanera-guests-stay-home-due-to-recent-immigration-story/1.png",
+      "stories/quinceanera-guests-stay-home-due-to-recent-immigration-story/2.png",
+      "stories/quinceanera-guests-stay-home-due-to-recent-immigration-story/3.png",
+      "stories/quinceanera-guests-stay-home-due-to-recent-immigration-story/4.png",
+      "stories/quinceanera-guests-stay-home-due-to-recent-immigration-story/5.png",
+      "stories/quinceanera-guests-stay-home-due-to-recent-immigration-story/6.png",
+      "stories/quinceanera-guests-stay-home-due-to-recent-immigration-story/7.png",
+      "stories/quinceanera-guests-stay-home-due-to-recent-immigration-story/8.png",
+      "stories/quinceanera-guests-stay-home-due-to-recent-immigration-story/9.png",
+      "stories/quinceanera-guests-stay-home-due-to-recent-immigration-story/10.png",
+      "stories/quinceanera-guests-stay-home-due-to-recent-immigration-story/11.png",
+      "stories/quinceanera-guests-stay-home-due-to-recent-immigration-story/12.png",
+      "stories/quinceanera-guests-stay-home-due-to-recent-immigration-story/13.png",
+      "stories/quinceanera-guests-stay-home-due-to-recent-immigration-story/14.png",
+      "stories/quinceanera-guests-stay-home-due-to-recent-immigration-story/15.png"
+    ],
+    featured: false,
+    isNewest: false,
+    title: "Quinceañera Guests Stay Home Due to Recent Immigration",
+    summary: "Today, we are sharing the story of a Quinceañera that was affected by recent immigration enforcement in Rowland Heights. Denise Alvarez was getting ready to celebrate her Sweet 16 through a traditional Latino party called a Quinceañera, a milestone celebration that honored her transition from girlhood into adulthood. Her family spent months preparing for the event and invested more than $15,000 to make the day as special as possible. Alvarez was excited to show off her dress to the more than 200 guests invited to celebrate with her.\n\nJust days before the Quinceañera, immigration enforcement was reported to be in the area, causing many guests to cancel. The family not only lost a significant amount of money, but also experienced emotional, mental, and cultural harm. We believe immigrants should not have to live in fear of stepping outside their homes because immigration enforcement is nearby. People should not be scared of being abducted and detained because of their race or status. The source we used for this post comes from NBC LA. If you would like to learn more about Denise Alvarez’s Quinceañera and the details of her story, we encourage you to read the reporting they published.",
+    tiktokLink: "https://www.tiktok.com/@projectsecondvoice/photo/7526423315565645111?is_from_webapp=1&sender_device=pc",
+    link: "stories.html#quinceanera-guests-stay-home-due-to-recent-immigration-story"
   }
 ];
 
